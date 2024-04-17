17d34c9143043e1a46a6bc8b6e3f0c5ddc5a6658
Alex <alex@settlemyrenursery.com>
2024-04-17 10:33:52 -0400

    Add RabbitMQ queue for design info processing

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 import urllib.parse
 from datetime import datetime
 from email import utils
+import pika
+import sys
 
 import flask
 import pandas
@@ -28,137 +30,21 @@ CORS(app)
 dev = False
 
 # When True, disables sms text and automatic printing in office
-test_mode = True
+test_mode = False
 
 
 @app.route('/design', methods=["POST"])
 def get_service_information():
-    """Route for information request about company service.
-    Sends user an email with PDF attachment.
-    Sends sales manager lead info in sms text.
-    Prints lead info in office.
-    Adds lead details to Google Sheet
-    Logs lead details for use in chron jobs.
-    and personalized details."""
+    """Route for information request about company service. Sends JSON to RabbitMQ for processing."""
     data = request.json
-    first_name = data.get('first_name')
-    last_name = data.get('last_name')
-    email = data.get('email')
-    phone = sms_engine.format_phone(data.get('phone'), mode='counterpoint')
-    timeline = data.get('timeline')
-    interested_in = data.get('interested_in')
-    street = data.get('street')
-    city = data.get('city')
-    state = data.get('state') if data.get('state') != 'State' else ""
-    zip_code = data.get('zip_code')
-    comments = data.get('comments')
-    # Concat the address
-    address = f"{street}, {city}, {state}, {zip_code}"
-
-    # Concatenate user interests (for text and spreadsheet use)
-    interests = ""
-    if interested_in is not None:
-        for x in interested_in:
-            interests += x
-            if len(interested_in) > 1:
-                interests += ", "
-        if len(interested_in) > 1:
-            # remove last trailing characters (", ")
-            interests = interests[:-2]
-
-    # Log Details
-    # establish time for consistent logging
-    now = datetime.now()
-
-    design_lead_data = [[str(now)[:-7], first_name, last_name, email, phone, interested_in, timeline,
-                         street, city, state, zip_code, comments]]
-    df = pandas.DataFrame(design_lead_data,
-                          columns=["date", "first_name", "last_name", "email", "phone", "interested_in", "timeline",
-                                   "street", "city", "state", "zip_code", "comments"])
-    log_engine.write_log(df, creds.lead_log)
-
-    # Send text notification To sales team manager
-    if not test_mode:
-        try:
-            sms_engine.design_text(first_name, last_name, phone, interests, timeline, address, comments)
-        except Exception as err:
-            error_type = "sms"
-            error_data = [[str(now)[:-7], error_type, err]]
-            df = pandas.DataFrame(error_data, columns=["date", "error_type", "message"])
-            log_engine.write_log(df, f"{creds.lead_error_log}/error_{now.strftime("%m_%d_%y_%H_%M_%S")}.csv")
-            print(err)
-
-    # Send email to client
-    try:
-        email_engine.design_email(first_name, email)
-    except Exception as err:
-        error_type = "email"
-        error_data = [[str(now)[:-7], error_type, err]]
-        df = pandas.DataFrame(error_data, columns=["date", "error_type", "message"])
-        log_engine.write_log(df, f"{creds.lead_error_log}/error_{now.strftime("%m_%d_%y_%H_%M_%S")}.csv")
-        print(err)
-
-    # Print lead details for in-store use
-
-    # Create the Word document
-    try:
-        doc = DocxTemplate("templates/lead_template.docx")
-
-        context = {
-            # Product Details
-            'date': now.strftime("%m/%d/%Y %H:%M %p"),
-            'name': first_name + " " + last_name,
-            'email': email,
-            'phone': phone,
-            'interested_in': interested_in,
-            'timeline': timeline,
-            'address': address,
-            'comments': comments
-        }
-
-        doc.render(context)
-        ticket_name = f"lead_{now.strftime("%m_%d_%y_%H_%M_%S")}.docx"
-        # Save the rendered file for printing
-        doc.save(f"./{ticket_name}")
-        # Print the file to default printer
-        if not test_mode:
-            os.startfile(ticket_name, "print")
-        # Delay while print job executes
-        time.sleep(2)
-        # Delete the unneeded Word document
-        os.remove(ticket_name)
-    except Exception as err:
-        error_type = "lead_ticket"
-        error_data = [[str(now)[:-7], error_type, err]]
-        df = pandas.DataFrame(error_data, columns=["date", "error_type", "message"])
-        log_engine.write_log(df, f"{creds.lead_error_log}/error_{now.strftime("%m_%d_%y_%H_%M_%S")}.csv")
-
-    # Upload to sheety API for spreadsheet use
-
-    sheety_post_body = {
-        "sheet1": {
-            "date": str(now),
-            "first": first_name,
-            "last": last_name,
-            "phone": phone,
-            "email": email,
-            "interested": interests,
-            "timeline": timeline,
-            "street": street,
-            "city": city,
-            "state": state,
-            "zip": zip_code,
-            "comments": comments
-        }
-    }
-    try:
-        # Try block stands to decouple our implementation from API changes that might impact app.
-        requests.post(url=creds.sheety_design_url, headers=creds.sheety_header, json=sheety_post_body)
-    except Exception as err:
-        error_type = "spreadsheet"
-        error_data = [[str(now)[:-7], error_type, err]]
-        df = pandas.DataFrame(error_data, columns=["date", "error_type", "message"])
-        log_engine.write_log(df, f"{creds.lead_error_log}/error_{now.strftime("%m_%d_%y_%H_%M_%S")}.csv")
+    payload = json.dumps(data)
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='design_info')
+    channel.basic_publish(exchange='',
+                          routing_key='design_info',
+                          body=payload)
+    connection.close()
 
     return "Your information has been received. Please check your email for more information from our team."
 
@@ -284,88 +170,94 @@ def bc_orders():
     """Webhook route for incoming orders. Renders pick/loading ticket. Automatically prints"""
     response_data = request.get_json()
     order_id = response_data['data']['id']
+    now = datetime.now()
     # Create order object
-    order = Order(order_id)
+    try:
+        order = Order(order_id)
+        # Filter out DECLINED payments
+        if order.payment_status != 'declined' and order.payment_status != "":
+            bc_date = order.date_created
+            # Format Date and Time
+            dt_date = utils.parsedate_to_datetime(bc_date)
+            date = utc_to_local(dt_date).strftime("%m/%d/%Y")  # ex. 04/24/2024
+            time = utc_to_local(dt_date).strftime("%I:%M:%S %p")  # ex. 02:34:24 PM
+            products = order.order_products
+            product_list = []
+            gift_card_only = True
+            for x in products:
+                if x['type'] == 'physical':
+                    gift_card_only = False
+                item = product_engine.Product(x['sku'])
+                product_details = {'sku': item.item_no,
+                                   'name': item.descr,
+                                   'qty': x['quantity'],
+                                   'base_price': x['base_price'],
+                                   'base_total': x['base_total']
+                                   }
+                product_list.append(product_details)
 
-    # Filter out DECLINED payments
-    if order.payment_status != 'declined':
-        bc_date = order.date_created
-        # Format Date and Time
-        dt_date = utils.parsedate_to_datetime(bc_date)
-        date = utc_to_local(dt_date).strftime("%m/%d/%Y")  # ex. 04/24/2024
-        time = utc_to_local(dt_date).strftime("%I:%M:%S %p")  # ex. 02:34:24 PM
-        products = order.order_products
-        product_list = []
-        gift_card_only = True
-        for x in products:
-            if x['type'] == 'physical':
-                gift_card_only = False
-            item = product_engine.Product(x['sku'])
-            product_details = {'sku': item.item_no,
-                               'name': item.descr,
-                               'qty': x['quantity'],
-                               'base_price': x['base_price'],
-                               'base_total': x['base_total']
-                               }
-            product_list.append(product_details)
-
-        # FILTER OUT GIFT CARDS (NO PHYSICAL PRODUCTS)
-        if not gift_card_only:
-            # Create Barcode
-            barcode_filename = 'barcode'
-            barcode_engine.generate_barcode(data=order_id, filename=barcode_filename)
-            # Create the Word document
-            doc = DocxTemplate("./templates/ticket_template.docx")
-            barcode = InlineImage(doc, f'./{barcode_filename}.png', height=Mm(15))  # width in mm
-
-            context = {
-                # Company Details
-                'company_name': creds.company_name,
-                'co_address': creds.company_address,
-                'co_phone': creds.company_phone,
-                # Order Details
-                'order_number': order_id,
-                'order_date': date,
-                'order_time': time,
-                'order_subtotal': float(order.subtotal_inc_tax),
-                'order_shipping': float(order.shipping_cost_inc_tax),
-                'order_total': float(order.total_inc_tax),
-                # Customer Billing
-                'cb_name': order.billing_first_name + " " + order.billing_last_name,
-                'cb_phone': order.billing_phone,
-                'cb_email': order.billing_email,
-                'cb_street': order.billing_street_address,
-                'cb_city': order.billing_city,
-                'cb_state': order.billing_state,
-                'cb_zip': order.billing_zip,
-                # Customer Shipping
-                'shipping_method': order.shipping_method,
-                'cs_name': order.shipping_first_name + " " + order.shipping_last_name,
-                'cs_phone': order.shipping_phone,
-                'cs_email': order.shipping_email,
-                'cs_street': order.shipping_street_address,
-                'cs_city': order.shipping_city,
-                'cs_state': order.shipping_state,
-                'cs_zip': order.shipping_zip,
-                # Product Details
-                'number_of_items': order.items_total,
-                'ticket_notes': order.customer_message,
-                'products': product_list,
-                'coupon_code': order.order_coupons['code'],
-                'coupon_discount': float(order.coupon_discount),
-                'loyalty': float(order.store_credit_amount),
-                'gc_amount': float(order.gift_certificate_amount),
-                'barcode': barcode
-            }
-            doc.render(context)
-            ticket_name = f"ticket_{order_id}_{datetime.now().strftime("%m_%d_%y_%H_%M_%S")}.docx"
-            file_path = creds.ticket_location + ticket_name
-            doc.save(file_path)
-            # Print the file to default printer
-            os.startfile(file_path, "print")
-            # Delete barcode files
-            os.remove(f"./{barcode_filename}.png")
-            os.remove(f"./{order_id}.svg")
+            # FILTER OUT GIFT CARDS (NO PHYSICAL PRODUCTS)
+            if not gift_card_only:
+                # Create Barcode
+                barcode_filename = 'barcode'
+                barcode_engine.generate_barcode(data=order_id, filename=barcode_filename)
+                # Create the Word document
+                doc = DocxTemplate("./templates/ticket_template.docx")
+                barcode = InlineImage(doc, f'./{barcode_filename}.png', height=Mm(15))  # width in mm
+                context = {
+                    # Company Details
+                    'company_name': creds.company_name,
+                    'co_address': creds.company_address,
+                    'co_phone': creds.company_phone,
+                    # Order Details
+                    'order_number': order_id,
+                    'order_date': date,
+                    'order_time': time,
+                    'order_subtotal': float(order.subtotal_inc_tax),
+                    'order_shipping': float(order.shipping_cost_inc_tax),
+                    'order_total': float(order.total_inc_tax),
+                    # Customer Billing
+                    'cb_name': order.billing_first_name + " " + order.billing_last_name,
+                    'cb_phone': order.billing_phone,
+                    'cb_email': order.billing_email,
+                    'cb_street': order.billing_street_address,
+                    'cb_city': order.billing_city,
+                    'cb_state': order.billing_state,
+                    'cb_zip': order.billing_zip,
+                    # Customer Shipping
+                    'shipping_method': order.shipping_method,
+                    'cs_name': order.shipping_first_name + " " + order.shipping_last_name,
+                    'cs_phone': order.shipping_phone,
+                    'cs_email': order.shipping_email,
+                    'cs_street': order.shipping_street_address,
+                    'cs_city': order.shipping_city,
+                    'cs_state': order.shipping_state,
+                    'cs_zip': order.shipping_zip,
+                    # Product Details
+                    'number_of_items': order.items_total,
+                    'ticket_notes': order.customer_message,
+                    'products': product_list,
+                    'coupon_code': order.order_coupons['code'],
+                    'coupon_discount': float(order.coupon_discount),
+                    'loyalty': float(order.store_credit_amount),
+                    'gc_amount': float(order.gift_certificate_amount),
+                    'barcode': barcode
+                }
+                doc.render(context)
+                ticket_name = f"ticket_{order_id}_{datetime.now().strftime("%m_%d_%y_%H_%M_%S")}.docx"
+                file_path = creds.ticket_location + ticket_name
+                doc.save(file_path)
+                # Print the file to default printer
+                os.startfile(file_path, "print")
+                # Delete barcode files
+                os.remove(f"./{barcode_filename}.png")
+                os.remove(f"./{order_id}.svg")
+    except Exception as err:
+        error_type = "general catch"
+        error_data = [[str(now)[:-7], error_type, err]]
+        df = pandas.DataFrame(error_data, columns=["date", "error_type", "message"])
+        log_engine.write_log(df, f"{creds.lead_error_log}/general_error_{now.strftime("%m_%d_%y_%H_%M_%S")}.csv")
+        print(err)
 
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
